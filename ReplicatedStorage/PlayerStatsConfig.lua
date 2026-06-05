ff1c86208bce3a29dddf2d205cf9d8c96bee5fdc
MeowDeRiWay <defer1366@gmail.com>
2026-06-05 22:06:12 +0300

    Add files via upload

---
+++ ReplicatedStorage/PlayerStatsConfig.lua
@@ -0,0 +1,12 @@
+local PlayerStatsConfig = {
+	MaxHealth = 20,
+	MaxStamina = 100,
+
+	NormalSpeed = 5,
+	SprintSpeed = 15,
+
+	StaminaDrainPerSecond = 10,
+	StaminaRegenPerSecond = 3,
+}
+
+return PlayerStatsConfig
