5099309737dd4fc968d76be5bb4f9b64402118c9
MeowDeRiWay <defer1366@gmail.com>
2026-06-25 15:26:35 +0300

    Update BaseObjectHandler.lua

--- ServerScriptService/ResourceModules/BaseObjectHandler.lua
+++ ServerScriptService/ResourceModules/BaseObjectHandler.lua
@@ -1,23 +1,33 @@
 local BaseObjectHandler = {}
 
-local BaseObjectPlace = require(script:WaitForChild("BaseObjectPlace"))
-local BaseObjectInteract = require(script:WaitForChild("BaseObjectInteract"))
+local context = nil
 
-function BaseObjectHandler.Init(context)
-	BaseObjectPlace.Init(context)
-	BaseObjectInteract.Init(context)
+function BaseObjectHandler.Init(ctx)
+	context = ctx
 end
 
 function BaseObjectHandler.Handle(player, action, data, itemName)
+	if not context then
+		warn("[BaseObjectHandler] context is nil")
+		return
+	end
+
 	if action == "Place" then
-		BaseObjectPlace.Run(player, data, itemName)
+		context.BaseObjectPlace.Run(player, data, itemName)
 		return
 	end
 
 	if action == "Interact" then
-		BaseObjectInteract.Run(player, data)
+		context.BaseObjectInteract.Run(player, data, itemName)
 		return
 	end
+
+	if action == "Damage" then
+		context.ObjectHitHandler.Handle(player, data, itemName)
+		return
+	end
+
+	warn("[BaseObjectHandler] Unknown action:", action)
 end
 
 return BaseObjectHandler
